84606a49b1008c33817d6c2de9f32c20b9e00c05
Egor <egor.fokin.05.05@mail.ru>
2025-02-06 13:17:59 +0300

    task 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+// task 1
+// определите только функцию power, где
+//    x - число, которое нужно возвести в степень
+//    p - степень, в которую нужно возвести x
+// (Напишите функцию power, реализующую возведение целого числа в неотрицательную целую степень.)
 
-int main()
-{
-    std::cout << "Hello, World" << std::endl;
+int power(int x, int p) {
+  int answer = 1;
+  if (p == 0){
+      answer = 1;
+  }
+  for(int i = 0; i < p; i++)
+    answer *= x;
+
+  return answer;
+}
+
+int main (){
+    std::cout << power(4,4)<<std::endl;
 }
